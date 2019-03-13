04f154cca9ca94e1f6deb0d510726e612ed350ed
zhenzhen lin <linzhenzhen@smartisan.com>
2019-03-13 16:33:24 +0800

    feat: 利用 ExtractTextPlugin 从js中抽离css

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin'); // 这个插件每次会
 var webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin'); // 文件压缩
 const CleanWebpackPlugin = require('clean-webpack-plugin'); // 每次打包时候 clean dist
+const ExtractTextPlugin = require('extract-text-webpack-plugin'); // 抽取css css生成自己的文件，不和js在一块了
 
 module.exports = {
   /* 入口 */
@@ -44,8 +45,12 @@ module.exports = {
         include: path.join(__dirname, 'src')
       },
       {
-        test: /\.css/,
-        use: ['style-loader', 'css-loader']
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({ // 从 js 中抽离 css
+          fallback: 'style-loader',
+          use: 'css-loader'
+        }) 
+        //['style-loader', 'css-loader']
       },
       {
         test: /\.(png|jpg|jpeg|gif)/,
@@ -66,6 +71,10 @@ module.exports = {
       filename: 'index.html',
       template: path.join(__dirname, './src/index.html')
     }),
+    new ExtractTextPlugin({
+      filename: '[name].[contenthash:5].css',
+      allChunks: true
+    }),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor'
     }),
@@ -74,14 +83,14 @@ module.exports = {
         drop_console: true
       }
     }),
+    // 注：CommonsChunkPlugin 的 'vendor' 实例，必须在 'runtime' 实例之前引入
+    new webpack.HashedModuleIdsPlugin({ // 让 vendor 不随着页面 build 而重新 build 从而进行缓存
+      name: 'runtime'
+    }),
     new webpack.DefinePlugin({ // webpack 内置的 DefinePlugin 为所有的依赖定义这个环境变量
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
       }
-    }),
-    // 注：CommonsChunkPlugin 的 'vendor' 实例，必须在 'runtime' 实例之前引入
-    new webpack.HashedModuleIdsPlugin({ // 让 vendor 不随着页面 build 而重新 build 从而进行缓存
-      name: 'runtime'
     })
   ],
 
