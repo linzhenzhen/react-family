95f71d0023496986af5500f0327e7e56038e2f54
zhenzhen lin <linzhenzhen@smartisan.com>
2019-03-13 17:39:39 +0800

    feat: 接入 axios promise http

--- src/redux/actions/userInfo.js
+++ src/redux/actions/userInfo.js
@@ -3,40 +3,53 @@ export const GET_USER_INFO_REQUEST = "userInfo/GET_USER_INFO_REQUEST";
 export const GET_USER_INFO_SUCCESS = "userInfo/GET_USER_INFO_SUCCESS";
 export const GET_USER_INFO_FAIL = "userInfo/GET_USER_INFO_FAIL";
 
-function getUserInfoRequest() {
-  return {
-    type: GET_USER_INFO_REQUEST
-  }
-}
+// function getUserInfoRequest() {
+//   return {
+//     type: GET_USER_INFO_REQUEST
+//   }
+// }
 
-function getUserInfoSuccess(userInfo) {
-  return {
-    type: GET_USER_INFO_SUCCESS,
-    userInfo
-  }
-}
+// function getUserInfoSuccess(userInfo) {
+//   return {
+//     type: GET_USER_INFO_SUCCESS,
+//     userInfo
+//   }
+// }
 
-function getUserInfoFail() {
-  return {
-    type: GET_USER_INFO_FAIL
-  }
-}
+// function getUserInfoFail() {
+//   return {
+//     type: GET_USER_INFO_FAIL
+//   }
+// }
 
 // 运用 redux-thunk 作为中间件来处理 action 函数
-export function getUserInfo() {
-  return function(dispatch) {
-    dispatch(getUserInfoRequest());
+// export function getUserInfo() {
+//   return function(dispatch) {
+//     dispatch(getUserInfoRequest());
 
-    // 这里 user.json 的文件被 CleanWebpackPlugin 清理 dist 重新生成 dist 后删除了，为了测试，可以手动在新建一下
-    return fetch('http://localhost:8080/api/user.json')
-    .then(response => {
-      return response.json()
-    })
-    .then(json => {
-      dispatch(getUserInfoSuccess(json))
-    })
-    .catch(() => {
-      dispatch(getUserInfoFail())
-    })
+//     // 这里 user.json 的文件被 CleanWebpackPlugin 清理 dist 重新生成 dist 后删除了，为了测试，可以手动在新建一下
+//     return fetch('http://localhost:8080/api/user.json')
+//     .then(response => {
+//       return response.json()
+//     })
+//     .then(json => {
+//       dispatch(getUserInfoSuccess(json))
+//     })
+//     .catch(() => {
+//       dispatch(getUserInfoFail())
+//     })
+//   }
+// }
+
+export function getUserInfo() {
+  return {
+    types: [GET_USER_INFO_REQUEST, GET_USER_INFO_SUCCESS, GET_USER_INFO_FAIL],
+    promise: client => client.get(`http://localhost:8080/api/user.json`)  // 这里需要手动新建 dist/api/user.json
   }
 }
+
+// dist/api/user.json
+// {
+//   "name": "mslinzz",
+//   "intro": "i am a coder.nm "
+// }
