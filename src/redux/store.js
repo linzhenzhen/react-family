d186c78f8a9bf80ef9b56513fd96635dd508c256
zhenzhen lin <linzhenzhen@smartisan.com>
2019-03-14 14:47:43 +0800

    feat: redux 模块热替换配置

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,4 +7,11 @@ import promiseMiddleware from './middleware/promiseMiddleware';
 // let store = createStore(combineReducers, applyMiddleware(thunkMiddleware));
 let store = createStore(combineReducers, applyMiddleware(promiseMiddleware));
 
+if (module.hot) {
+  module.hot.accept("./reducers", () => {
+    const nextCombineReducers = require("./reducers").default;
+    store.replaceReducer(nextCombineReducers)
+  })
+}
+
 export default store;
